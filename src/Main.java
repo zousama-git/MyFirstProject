d20335afcc63895a2084dcb8545a4f3e11188436
zousama <mbekezeli071@gmail.com>
2026-03-18 21:32:10 +0200

    Java Day 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //This is my first java project
+        /*
+        This is a multiline comment
+         */
+        System.out.print("I am zou!\n");
+        System.out.println("I am the real zousama!");
+        System.out.print("Buy me pizza");
+        //type system 'sout' for fast System.out.println();
+    }
+}
