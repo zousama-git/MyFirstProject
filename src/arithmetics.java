23c111c9592111f3e6adc8cf3cb2193aa8e191bd
zousama <mbekezeli071@gmail.com>
2026-03-22 16:29:25 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/arithmetics.java
@@ -0,0 +1,58 @@
+public class arithmetics {
+    static void main(String[] args) {
+
+        /* //Arithmetic Operators
+        int x, y;
+        int add, sub, multi, div, mod;
+
+        x= 10;
+        y= 2;
+
+        add=x+y;
+        sub=x-y;
+        multi=x*y;
+        div=x/y;
+
+        //takes the remainder
+        mod=x%6;
+
+        System.out.println("Arithmetic Operators");
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+        System.out.println(mod); */
+
+        /* //Augmented Assignment Operators
+
+        int x, y;
+        //x+=y;
+        //x-=y;
+        //x*=y;
+        //x/=y;
+        //x%=y;
+
+        System.out.println("Augmented Assignment Operators");
+        System.out.println(x); */
+
+        //Increment and decrement Operators
+        /* int x=1;
+
+        //Increment
+        //x=x+1;
+        //x+=1;
+        //x++;
+
+        //decrement
+        x--;
+        x--;
+
+        System.out.println(x); */
+
+        //Oder of operations bod mas
+
+        double results=3+4*(7-5)/2.0;
+        System.out.println(results);
+
+    }
+}
